a6cd6f3e8d3924f94f54fa8fbe9a0016931e9017
unknown <hoangnguyen164@gmail.com>
2025-08-31 03:05:57 +0700

    first commit

---
+++ src/main/java/com/hn369/universeblog/UniverseBlogApplication.java
@@ -0,0 +1,13 @@
+package com.hn369.universeblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniverseBlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UniverseBlogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hn369/universeblog/UniverseBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hn369.universeblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UniverseBlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
