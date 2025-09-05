52950eb164cdb25d7c02f2964a6236f963a7ac21
unknown <hoangnguyen164@gmail.com>
2025-09-06 03:24:34 +0700

    upate Cors config

--- src/main/java/com/hn369/universeblog/app/configuration/CorsConfig.java
+++ src/main/java/com/hn369/universeblog/app/configuration/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:3000", "https://universe-blog-service.onrender.com")
+                        .allowedOrigins("http://localhost:3000", "https://online-code-formatter.com")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true)
