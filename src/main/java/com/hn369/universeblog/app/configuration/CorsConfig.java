cc9649876f784617671a0d503605e64fafbb8df3
unknown <hoangnguyen164@gmail.com>
2025-09-06 03:03:23 +0700

    add Cors config

---
+++ src/main/java/com/hn369/universeblog/app/configuration/CorsConfig.java
@@ -0,0 +1,69 @@
+package com.hn369.universeblog.app.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+    
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("http://localhost:3000", "https://universe-blog-service.onrender.com")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true)
+                        .maxAge(3600);
+            }
+        };
+    }
+}
+
+/*
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+    
+    @Value("${cors.allowed.origins}")
+    private String[] allowedOrigins;
+    
+    @Value("${cors.allowed.methods}")
+    private String[] allowedMethods;
+    
+    @Value("${cors.allowed.headers}")
+    private String[] allowedHeaders;
+    
+    @Value("${cors.allow-credentials}")
+    private boolean allowCredentials;
+    
+    @Value("${cors.max-age}")
+    private long maxAge;
+    
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins(allowedOrigins)
+                        .allowedMethods(allowedMethods)
+                        .allowedHeaders(allowedHeaders)
+                        .allowCredentials(allowCredentials)
+                        .maxAge(maxAge);
+            }
+        };
+    }
+}
+
+ * */
